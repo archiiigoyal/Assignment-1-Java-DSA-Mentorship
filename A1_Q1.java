7dc6ce3b53198ab72b7958b2bc6fe4bb070536ad
ARCHI GOYAL <144723374+archiiigoyal@users.noreply.github.com>
2023-10-06 19:57:03 +0530

    Print 'Z'.java

---
+++ A1_Q1.java
@@ -0,0 +1,11 @@
+package Assignment1DSAMentorship;
+
+public class A1_Q1 {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
